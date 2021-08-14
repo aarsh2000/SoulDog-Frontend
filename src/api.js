d90551980ef802b399bb40c0c8aa1e55b51f67dc
Aarsh <aarsh937@gmail.com>
2021-08-13 23:01:19 -0400

    heroku

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://souldog-api.herokuapp.com/';
 
 const login = (form) =>
   axios.post(`${API_URL}/user/login`, { username: form.email, password: form.password });
